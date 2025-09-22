5381760ad48c52d806acddd67abf02e476c3c41f
r6915ee <astergthefourth@gmail.com>
2025-09-21 20:56:05 -0700

    feat: add save system

--- src/core.rs
+++ src/core.rs
@@ -11,10 +11,29 @@ pub mod graphics;
 pub mod data;
 
 use bevy::prelude::*;
+use data::*;
+use std::io::ErrorKind;
 
 /// Basic sample system.
 pub fn hello_system() {
     println!("Hello, world!");
+}
+
+/// System responsible for creating save data.
+fn save_system() {
+    match generate_save_dir() {
+        Ok(_) => (),
+        Err(e) => match e.kind() {
+            ErrorKind::AlreadyExists => (),
+            ErrorKind::NotFound => panic!("{}", e),
+            _ => (),
+        },
+    }
+    let settings: Settings = read_settings();
+    println!(
+        "Parsed settings: {}",
+        ron::ser::to_string_pretty(&settings, ron::ser::PrettyConfig::default()).unwrap()
+    );
 }
 
 /// Provides most systems and components directly.
@@ -27,5 +46,6 @@ pub struct FunkAspectPlugin;
 impl Plugin for FunkAspectPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, hello_system);
+        app.add_systems(Startup, save_system);
     }
 }
